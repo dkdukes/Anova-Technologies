8a16db6b2b06617e44f38af2dc09c3665a37605b
dkdukes <fedulike@gmail.com>
2026-08-15 14:47:25 +0300

    feat: create views for the product app

--- products/views.py
+++ products/views.py
@@ -1,3 +1,98 @@
 from django.shortcuts import render
+from rest_framework import generics, filters
+from . models import (
+    Brand,
+    Category,
+    Product,
+)
+from . serializers import (
+    BrandSerializer,
+    CategorySerializer,
+    ProductSerializer,
+)
 
 # Create your views here.
+class CategoryListAPIView(generics.ListAPIView):
+    queryset = Category.objects.filter(is_active = True)
+    serializer_class = CategorySerializer
+    filter_backends = [
+        filters.SearchFilter,
+    ]
+    search_fields = [
+        "name",
+    ]
+
+class BrandListAPIView(generics.ListAPIView):
+    queryset = Brand.objects.filter(is_active = True)
+    serializer_class = BrandSerializer
+    filter_backends = [
+        filters.SearchFilter,
+    ]
+    filter_fields = [
+        "name",
+    ]
+
+
+class ProductListAPIView(generics.ListAPIView):
+
+    queryset = (
+        Product.objects
+        .filter(status="active")
+        .select_related(
+            "category",
+            "brand",
+        )
+        .prefetch_related(
+            "images",
+            "specifications",
+        )
+    )
+
+    serializer_class = ProductSerializer
+
+    filter_backends = [
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+
+    search_fields = [
+        "name",
+        "sku",
+        "short_description",
+        "description",
+        "brand__name",
+        "category__name",
+    ]
+
+    ordering_fields = [
+        "price",
+        "created_at",
+        "name",
+        "stock_quantity",
+    ]
+
+    ordering = [
+        "-created_at",
+    ]
+
+
+class ProductDetailAPIView(
+    generics.RetrieveAPIView
+):
+
+    queryset = (
+        Product.objects
+        .filter(status="active")
+        .select_related(
+            "category",
+            "brand",
+        )
+        .prefetch_related(
+            "images",
+            "specifications",
+        )
+    )
+
+    serializer_class = ProductSerializer
+
+    lookup_field = "slug"
