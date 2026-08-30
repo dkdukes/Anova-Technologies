714581e857c10ea7af696aca9ca6035f31f8e4ea
dkdukes <fedulike@gmail.com>
2026-08-30 18:32:05 +0300

    feat: create the view methods that execute the transactions

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -19,7 +21,62 @@ class MpesaTestView(APIView):
 
         except Exception as error:
 
-            return Response({
-                "success": False,
-                "error": str(error),
-            }, status=500)
+            return Response(
+                {
+                    "success": False,
+                    "error": str(error),
+                },
+                status=500,
+            )
+
+
+class MpesaStkTestView(APIView):
+
+    def post(self, request):
+
+        phone_number = request.data.get(
+            "phone_number"
+        )
+
+        amount = request.data.get(
+            "amount"
+        )
+
+        if not phone_number:
+            return Response(
+                {
+                    "error": "Phone number is required."
+                },
+                status=400,
+            )
+
+        if not amount:
+            return Response(
+                {
+                    "error": "Amount is required."
+                },
+                status=400,
+            )
+
+        try:
+
+            response = MpesaService.stk_push(
+                phone_number=phone_number,
+                amount=Decimal(amount),
+                account_reference="ANOVA-TEST",
+                transaction_description=(
+                    "Anova Technologies Test Payment"
+                ),
+            )
+
+            return Response(response)
+
+        except Exception as error:
+
+            return Response(
+                {
+                    "success": False,
+                    "error": str(error),
+                },
+                status=500,
+            )
